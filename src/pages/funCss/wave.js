b350644e06dbc0ce86a6ce5e2e0bbdb9d4deb2e8
李林城 <1019034271@qq.com>
2021-05-20 16:44:07 +0800

    feat: css 实现 ‘波浪’

--- src/pages/funCss/wave.js
+++ src/pages/funCss/wave.js
@@ -1,11 +1,170 @@
 import React , { useState , useEffect , forwardRef} from 'react';
 import ItemBox from "../../components/itemBox";
 import style from "./style/wave.scss";
+
 export default forwardRef(( props , ref) => {
     return <React.Fragment>
-        <div className={style.className}>
-            <ItemBox title="纯css 实现波浪">
-                
+        <div className={style.wave}>
+            <ItemBox title="css 利用 圆和旋转 实现波浪">
+                <div className="rect">
+                    <div className="circle c1"></div>
+                    <div className="circle c2"></div>
+                    {/* <div className="circle c3"></div> */}
+                </div>
+            </ItemBox>
+            <ItemBox title="css 利用多个div高度变换 实现波浪">
+                <div className="g-rect">
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+                    <div className="g-item"></div>
+
+                </div>
             </ItemBox>
         </div>
     </React.Fragment>
